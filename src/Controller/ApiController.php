6a2cdbb0df1304eb25293208bfbe31527e6631ab
SIMON-5000 <sidr24@student.bth.com>
2025-04-02 22:16:24 +0200

    add Routes for kmom01

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController
+{
+    #[Route("/api")]
+    public function allApi(): Response
+    {
+        // $router = $this->get('router');
+        // $routeList = $router->getRouteCollection()->all();
+        $routeList = [
+            'quote' => '/api/quote'
+        ];
+
+        return new JsonResponse($routeList);
+    }
+
+
+    #[Route("/api/quote", name:"quote")]
+    public function jsonQuotes(): Response
+    {
+        $number = random_int(0, 52);
+        $quotes = json_decode(file_get_contents( 'data/quotes.json'), true);
+        // var_dump($quotes['quotes']);
+        // var_dump(array_keys($quotes));
+        $quote = $quotes['quotes'][$number];
+
+        // Returning JSON data:
+
+        // $response = new Response();
+        // $response->setContent(json_encode($data));
+        // // Sets type of data
+        // $response->headers->set('Content-Type', 'application/json');
+        // return $response;
+
+        // This can be simplified by using: JsonResponse
+        // "imported" with use at top.
+
+
+        // RAW JSON DATA TO PRINT
+        // return new JsonResponse($quotes);
+        
+        // Here we first prettify the json data, JSON_PRETTY_PRINT
+        
+        $response = new JsonResponse($quote);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
---
+++ src/Controller/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Holds the main routes for the page.
+ */
+class MainController extends AbstractController
+{
+    #[Route('/', name: "me")]
+    public function number(): Response
+    {
+        return $this->render('me.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function home(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+}
